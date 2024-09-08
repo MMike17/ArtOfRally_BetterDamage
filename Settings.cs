e40443a22ce6d7e00cfe23cbbaab44878cbc1077
Mike <matthews.r.mike@gmail.com>
2024-09-08 13:01:44 +0200

    Add settings for landing damage

--- Settings.cs
+++ Settings.cs
@@ -1,4 +1,6 @@
+using UnityEngine;
 using UnityModManagerNet;
+
 using static UnityModManagerNet.UnityModManager;
 
 namespace BetterDamage
@@ -6,6 +8,19 @@ namespace BetterDamage
     public class Settings : ModSettings, IDrawable
     {
         // [Draw(DrawType.)]
+
+        [Header("Landing")]
+        [Draw(DrawType.Toggle)]
+        public bool enableLandingDamage;
+
+        [Draw(DrawType.Slider, Min = 0, Max = 10, VisibleOn = "enableLandingDamage|true")] // TODO : Decide Min and Max values here
+        public float landingThreshold; // TODO : Decide default value here
+
+        [Draw(DrawType.Slider, Min = 0, Max = 100, VisibleOn = "enableLandingDamage|true")]
+        public float landingPunctureProbability = 0.5f; // TODO : Decide default value here
+
+        // TODO : Add puncture probability on crash
+        // TODO : Add headlight damage probability on crash
 
         public override void Save(ModEntry modEntry) => Save(this, modEntry);
 
