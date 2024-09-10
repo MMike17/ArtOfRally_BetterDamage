eccef458402048c83f234b45339475fdee4e52aa
Mike <matthews.r.mike@gmail.com>
2024-09-10 23:05:15 +0200

    Add tire puncture probability on drift

--- Patches.cs
+++ Patches.cs
@@ -1,6 +1,7 @@
 using HarmonyLib;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
@@ -27,10 +28,8 @@ namespace BetterDamage
 
     // TARGETS :
 
-    // TODO : damage tires when you drift (detect when we drift/slip / chance of puncture)
     // TODO : damage gearbox when you shift down and over rev / shift R when going forward / shift 1 when going back(complex to detect)
     // TODO : damage engine whe you over rev (detect over rev)
-    // TODO : damage suspensions when bump (detect the direction of bump
     // TODO : damage turbo when overheat (detect when we overheat ? compare rev to forward speed / rev > ProjectForward(maxSpeed / 10))
 
     [HarmonyPatch(typeof(PlayerCollider), "CheckForPunctureAndPerformanceDamage")]
@@ -304,8 +303,6 @@ namespace BetterDamage
             if (!Main.enabled || !(__instance is SteeringPerfomanceDamage))
                 return;
 
-            Main.Log("Detected repair part");
-
             Main.Try(() =>
             {
                 Main.SetField<float, SteeringPerfomanceDamage>(
@@ -317,6 +314,113 @@ namespace BetterDamage
 
                 SteeringDamageManager.Reset();
             });
+        }
+    }
+
+    [HarmonyPatch(typeof(StageScreen), nameof(StageScreen.Restart))]
+    static class RestartStagePatch
+    {
+        static void Postfix()
+        {
+            Main.Try(() => DriftManager.Reset());
+        }
+    }
+
+    [HarmonyPatch(typeof(PreStageScreen), nameof(PreStageScreen.StartStage))]
+    static class StartStagePatch
+    {
+        static void Postfix()
+        {
+            Main.Try(() => DriftManager.Reset());
+        }
+    }
+
+    [HarmonyPatch(typeof(Wheel), "FixedUpdate")]
+    static class DriftManager
+    {
+        const float DRIFT_SLIP_THRESHOLD = 0.9f;
+        const float DRIFT_DURATION_THRESHOLD = 20;
+        const float DRIFT_PUNCTURE_COOLDOWN = 15;
+
+        static Dictionary<Wheel, float> wheelDriftDuration;
+        static Dictionary<Wheel, bool> wheelInCooldown;
+
+        static void Postfix(Wheel __instance)
+        {
+            if (!Main.settings.enableDriftDamage)
+                return;
+
+            Main.Try(() =>
+            {
+                if (wheelDriftDuration == null)
+                {
+                    wheelDriftDuration = new Dictionary<Wheel, float>();
+                    wheelInCooldown = new Dictionary<Wheel, bool>();
+                }
+
+                if (!wheelDriftDuration.ContainsKey(__instance))
+                {
+                    wheelDriftDuration.Add(__instance, 0);
+                    wheelInCooldown.Add(__instance, false);
+
+                    // clean up dictionary
+                    foreach (Wheel wheel in wheelDriftDuration.Keys.ToArray())
+                    {
+                        if (wheel == null)
+                        {
+                            wheelDriftDuration.Remove(wheel);
+                            wheelInCooldown.Remove(wheel);
+                        }
+                    }
+                }
+
+                if (__instance.lateralSlip * (__instance.lateralSlip > 0 ? 1 : -1) >= DRIFT_SLIP_THRESHOLD)
+                    wheelDriftDuration[__instance] += Time.fixedDeltaTime;
+
+                if (wheelInCooldown[__instance])
+                {
+                    if (wheelDriftDuration[__instance] >= DRIFT_PUNCTURE_COOLDOWN)
+                    {
+                        wheelDriftDuration[__instance] -= DRIFT_PUNCTURE_COOLDOWN;
+                        wheelInCooldown[__instance] = false;
+                    }
+                }
+                else if (wheelDriftDuration[__instance] >= DRIFT_DURATION_THRESHOLD)
+                {
+                    wheelDriftDuration[__instance] -= DRIFT_DURATION_THRESHOLD;
+
+                    if (Random.Range(0f, 100f) < Main.settings.driftPunctureProbability)
+                    {
+                        // check if another tire is punctured
+                        bool hasPuncture = false;
+
+                        foreach (Wheel wheel in wheelDriftDuration.Keys)
+                        {
+                            if (wheel.tirePuncture)
+                            {
+                                hasPuncture = true;
+                                break;
+                            }
+                        }
+
+                        if (!hasPuncture)
+                        {
+                            CarUtils.PunctureTire(
+                                GameEntryPoint.EventManager.playerManager.PlayerObject.GetComponent<PlayerCollider>(),
+                                __instance
+                            );
+
+                            wheelInCooldown[__instance] = true;
+                        }
+                    }
+                }
+            });
+        }
+
+        public static void Reset()
+        {
+            wheelDriftDuration = null;
+            wheelInCooldown = null;
         }
     }
 }
